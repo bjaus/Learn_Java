5cce2df378113de82a588e4003b8c2c1165c4ac6
Brandon Jaus <brandon.jaus@gmail.com>
2016-03-31 07:42:12 -0500

    added PromDemo.java

---
+++ JavaABeginnersGuide6Ed/Chapter02/PromDemo.java
@@ -0,0 +1,17 @@
+// A promotion surprise
+class PromDemo {
+	public static void main(String args[]) {
+		byte b;
+		int i;
+		
+		b = 10;
+		i = b * b; // Ok, no cast needed
+		
+		System.out.println("i and b: " + i + " " + b);
+		
+		b = 10;
+		b = (byte) (b * b); // cast needed
+		
+		System.out.println("i and b: " + i + " " + b);
+	}
+}
