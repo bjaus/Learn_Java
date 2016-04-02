ad7b83f774bf82a3e71602db1e30340c52a5414f
Brandon <brandon.jaus@gmail.com>
2016-04-02 17:23:12 -0500

    added Power.java

--- JavaABeginnersGuide6Ed/Chapter03/Power.java
+++ JavaABeginnersGuide6Ed/Chapter03/Power.java
@@ -1,11 +1,12 @@
 // Compute integer powers of 2
 class Power {
 	public static void main(String args[]) {
-		int e;
-		int result;
+		int e, result;
+
 		for(int i = 0; i < 10; i++) {
 			result = 1;
 			e = i;
+			
 			while(e > 0) {
 				result *= 2;
 				e--;
